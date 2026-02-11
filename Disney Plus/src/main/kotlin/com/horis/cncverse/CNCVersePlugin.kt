851183c1180bef52aad26231c03dec5d6271566e
nyxarielqoventluxard32-max <nyxarielqoventluxard32@gmail.com>
2026-02-11 15:25:03 +0700

    Update CNCVersePlugin.kt

--- Disney Plus/src/main/kotlin/com/horis/cncverse/CNCVersePlugin.kt
+++ Disney Plus/src/main/kotlin/com/horis/cncverse/CNCVersePlugin.kt
@@ -5,11 +5,9 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-open class CNCVersePlugin: Plugin() {
+open class CNCVersePlugin : Plugin() {
     override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
-        NetflixMirrorStorage.init(context.applicationContext)
+        DisneyPlusStorage.init(context.applicationContext)
         registerMainAPI(DisneyPlusProvider())
     }
-
 }
